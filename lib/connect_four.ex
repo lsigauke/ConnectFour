83447ef567674096a8b85555bf964a4f8695d791
Lee Sigauke <leesigs@gmail.com>
2017-04-30 17:48:34 +0100

    Check pieces below and to the left of the current move

--- lib/connect_four.ex
+++ lib/connect_four.ex
@@ -1,18 +1,37 @@
 defmodule ConnectFour do
   @moduledoc """
-  Documentation for ConnectFour.
+  Connect four game
   """
+ def move(board, piece, column_index) do 
+  List.update_at(board, column_index, fn column ->
+      [piece | column] 
+  end)
+ end
 
-  @doc """
-  Hello world.
+ def winning_move?(board, column_index) do
+   case check_stacked(board, column_index) do
+     {:winner, nil} -> check_row_left(board, column_index)
+     winner -> winner
+   end
+ end
 
-  ## Examples
+ defp check_stacked(board, column_index) do
+  board
+  |> Enum.at(column_index)
+  |> is_four_in_succession?()
+ end
 
-      iex> ConnectFour.hello
-      :world
+ defp check_row_left(board, column_index) do
+  row_from_columns = generate_row_from_columns(board, column_index, -1, [])
+  is_four_in_succession?(row_from_columns)
+ end
+ 
+ defp generate_row_from_columns(_, current_index, _, acc) when current_index < 0, do: acc 
+ defp generate_row_from_columns(board, current_index, step_value, acc) do
+   current_column = Enum.at(board, current_index)
+   generate_row_from_columns(board, current_index + step_value, step_value, [hd(current_column) | acc])
+ end
 
-  """
-  def hello do
-    :world
-  end
+ defp is_four_in_succession?([p, p, p, p]) when not is_nil(p), do: {:winner, p}
+ defp is_four_in_succession?(_), do: {:winner, nil}
 end
--- test/connect_four_test.exs
+++ test/connect_four_test.exs
@@ -2,7 +2,59 @@ defmodule ConnectFourTest do
   use ExUnit.Case
   doctest ConnectFour
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do 
+    empty_board = [[],[],[],[],[],[],[],[],[]]
+    {:ok, empty_board: empty_board}
   end
+
+  test "can make a move", %{empty_board: board} do
+    updated_board = ConnectFour.move(board, :x, 0)
+    assert updated_board == [[:x],[],[],[],[],[],[],[],[]]
+  end
+  
+  test "only stacked four wins", %{empty_board: board} do
+    updated_board =  ConnectFour.move(board, :x, 0)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 0)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 0)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 0)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 0)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 0)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 0)
+    assert {:winner, :x} == ConnectFour.winning_move?(updated_board, 0)
+  end
+
+  test "only four in a row to the left wins at bottom", %{empty_board: board} do
+    updated_board =  ConnectFour.move(board, :x, 0)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 0)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 1)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 1)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 2)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 2)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    assert {:winner, :x} == ConnectFour.winning_move?(updated_board, 3)
+  end
+
+  test "only four in a row to the left wins anywhere", %{empty_board: board} do
+    updated_board =  ConnectFour.move(board, :x, 0)
+    updated_board =  ConnectFour.move(updated_board, :x, 0)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 0)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 1)
+    updated_board =  ConnectFour.move(updated_board, :x, 1)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 1)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 2)
+    updated_board =  ConnectFour.move(updated_board, :x, 2)
+    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 2)
+
+    updated_board =  ConnectFour.move(updated_board, :y, 3)
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    assert {:winner, :x} == ConnectFour.winning_move?(updated_board, 3)
+  end 
 end
