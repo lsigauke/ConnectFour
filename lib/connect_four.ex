01115f78454517cc38bf2f4285046fc35647b9a4
Lee Sigauke <leesigs@gmail.com>
2017-05-01 16:23:38 +0100

    Check for winning move from top right to bottom and from bottom to top right

--- lib/connect_four.ex
+++ lib/connect_four.ex
@@ -30,18 +30,41 @@ defmodule ConnectFour do
   end
  end
 
+ # TODO: Check diagonals from here
  def check_row_right(board, column_index) do
    row_from_columns = generate_row_from_columns(board, column_index, + 1, [])
    is_four_in_succession?(row_from_columns)
  end
 
  defp generate_row_from_columns(_, _, _, acc) when length(acc) == 4, do: acc 
- defp generate_row_from_columns(board, current_index, step, acc) do
-   current_column = Enum.at(board, current_index)
-   case length(current_column) do
-     0 -> acc
-     _ -> generate_row_from_columns(board, current_index + step, step, [hd(current_column) | acc])
-   end
+ defp generate_row_from_columns(board, current_column_index, shift, acc) do
+   current_column = Enum.at(board, current_column_index)
+   piece = Enum.at(current_column, 0)
+   next_column_index = current_column_index + shift
+   generate_row_from_columns(board, next_column_index, shift, [piece | acc])
+ end
+
+ def check_diagonal_to_bottom_left(board, column_index) do
+  current_column = Enum.at(board, column_index)
+  current_piece_index = div(Enum.count(current_column), -1)
+  row = generate_row_from_columns_diagonally(board, column_index, -1, +1, current_piece_index, [])
+  is_four_in_succession?(row)
+ end
+
+ def check_diagonal_to_top_right(board, column_index) do
+  current_column = Enum.at(board, column_index)
+  current_piece_index = div(Enum.count(current_column), -1)
+  row = generate_row_from_columns_diagonally(board, column_index, +1, -1, current_piece_index, [])
+  is_four_in_succession?(row)
+ end
+
+ defp generate_row_from_columns_diagonally(_, _, _, _, _, acc) when length(acc) == 4, do: acc
+ defp generate_row_from_columns_diagonally(board, column_index, column_shift, piece_shift, current_piece_index, acc) do
+  current_column = Enum.at(board, column_index)
+  piece = Enum.at(current_column, current_piece_index)
+  next_column_index = column_index + column_shift
+  next_piece_index = current_piece_index + piece_shift
+  generate_row_from_columns_diagonally(board, next_column_index, column_shift, piece_shift, next_piece_index, [piece | acc])
  end
 
  defp is_four_in_succession?([p, p, p, p]) when not is_nil(p), do: {:winner, p}
--- test/connect_four_test.exs
+++ test/connect_four_test.exs
@@ -7,12 +7,12 @@ defmodule ConnectFourTest do
     {:ok, empty_board: empty_board}
   end
 
-  test "can make a move", %{empty_board: board} do
+  test "make a move", %{empty_board: board} do
     updated_board = ConnectFour.move(board, :x, 0)
     assert updated_board == [[:x],[],[],[],[],[],[],[],[]]
   end
   
-  test "stacked four wins", %{empty_board: board} do
+  test "connect 4 stacked wins", %{empty_board: board} do
     updated_board =  ConnectFour.move(board, :x, 0)
     assert {:winner, nil} == ConnectFour.check_stacked(updated_board, 0)
 
@@ -26,7 +26,7 @@ defmodule ConnectFourTest do
     assert {:winner, :x} == ConnectFour.check_stacked(updated_board, 0)
   end
 
-  test "four in a row bottom going to the left wins", %{empty_board: board} do
+  test "connect 4 at bottom going to the left wins", %{empty_board: board} do
     updated_board =  ConnectFour.move(board, :x, 0)
     assert {:winner, nil} == ConnectFour.check_row_left(updated_board, 0)
 
@@ -40,25 +40,25 @@ defmodule ConnectFourTest do
     assert {:winner, :x} == ConnectFour.check_row_left(updated_board, 3)
   end
 
-  test "four in a row going to the left at any level wins", %{empty_board: board} do
+  test "connect 4 going to the left at any level wins", %{empty_board: board} do
     updated_board =  ConnectFour.move(board, :x, 0)
     updated_board =  ConnectFour.move(updated_board, :x, 0)
-    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 0)
+    assert {:winner, nil} == ConnectFour.check_row_left(updated_board, 0)
 
     updated_board =  ConnectFour.move(updated_board, :x, 1)
     updated_board =  ConnectFour.move(updated_board, :x, 1)
-    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 1)
+    assert {:winner, nil} == ConnectFour.check_row_left(updated_board, 1)
 
     updated_board =  ConnectFour.move(updated_board, :x, 2)
     updated_board =  ConnectFour.move(updated_board, :x, 2)
-    assert {:winner, nil} == ConnectFour.winning_move?(updated_board, 2)
+    assert {:winner, nil} == ConnectFour.check_row_left(updated_board, 2)
 
     updated_board =  ConnectFour.move(updated_board, :y, 3)
     updated_board =  ConnectFour.move(updated_board, :x, 3)
-    assert {:winner, :x} == ConnectFour.winning_move?(updated_board, 3)
+    assert {:winner, :x} == ConnectFour.check_row_left(updated_board, 3)
   end
 
-  test "four in a row going to the right at bottom wins", %{empty_board: board} do
+  test "connect 4 going to the right at bottom wins", %{empty_board: board} do
     updated_board =  ConnectFour.move(board, :x, 3)
     assert {:winner, nil} == ConnectFour.check_row_right(updated_board, 3)
 
@@ -72,7 +72,7 @@ defmodule ConnectFourTest do
     assert {:winner, :x} == ConnectFour.check_row_right(updated_board, 0)
   end 
 
-  test "four in a row going to the right at any level wins", %{empty_board: board} do
+  test "connect 4 going to the right at any level wins", %{empty_board: board} do
     updated_board =  ConnectFour.move(board, :x, 3)
     updated_board =  ConnectFour.move(updated_board, :x, 3)
     assert {:winner, nil} == ConnectFour.check_row_right(updated_board, 3)
@@ -88,5 +88,52 @@ defmodule ConnectFourTest do
     updated_board =  ConnectFour.move(updated_board, :y, 0)
     updated_board =  ConnectFour.move(updated_board, :x, 0)
     assert {:winner, :x} == ConnectFour.check_row_right(updated_board, 0)
+  end
+  
+  test "connect 4 diagonally to bottom left wins", %{empty_board: board} do
+    updated_board =  ConnectFour.move(board, :x, 0)
+    
+    assert {:winner, nil} == ConnectFour.check_diagonal_to_bottom_left(updated_board, 0)
+
+    updated_board =  ConnectFour.move(updated_board, :y, 1)
+    updated_board =  ConnectFour.move(updated_board, :x, 1)
+
+    assert {:winner, nil} == ConnectFour.check_diagonal_to_bottom_left(updated_board, 1)
+
+    updated_board =  ConnectFour.move(updated_board, :y, 2)
+    updated_board =  ConnectFour.move(updated_board, :y, 2)
+    updated_board =  ConnectFour.move(updated_board, :x, 2)
+
+    assert {:winner, nil} == ConnectFour.check_diagonal_to_bottom_left(updated_board, 2)
+    
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    updated_board =  ConnectFour.move(updated_board, :y, 3)
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    
+    assert {:winner, :x} == ConnectFour.check_diagonal_to_bottom_left(updated_board, 3)
+  end
+
+  test "connect 4 diagonally to top right wins", %{empty_board: board} do
+    updated_board =  ConnectFour.move(board, :x, 3)
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    updated_board =  ConnectFour.move(updated_board, :y, 3)
+    updated_board =  ConnectFour.move(updated_board, :x, 3)
+    
+    assert {:winner, nil} == ConnectFour.check_diagonal_to_top_right(updated_board, 3)
+    
+    updated_board =  ConnectFour.move(updated_board, :y, 2)
+    updated_board =  ConnectFour.move(updated_board, :y, 2)
+    updated_board =  ConnectFour.move(updated_board, :x, 2)
+
+    assert {:winner, nil} == ConnectFour.check_diagonal_to_top_right(updated_board, 2)
+
+    updated_board =  ConnectFour.move(updated_board, :y, 1)
+    updated_board =  ConnectFour.move(updated_board, :x, 1)
+
+    assert {:winner, nil} == ConnectFour.check_diagonal_to_top_right(updated_board, 1)
+
+    updated_board =  ConnectFour.move(updated_board, :x, 0)
+    assert {:winner, :x} == ConnectFour.check_diagonal_to_top_right(updated_board, 0)
   end
 end
