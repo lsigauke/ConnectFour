4b4d3dbf22647e76d459129a85f54dd407d9e85b
Lee Sigauke <leesigs@gmail.com>
2017-05-04 20:37:50 +0100

    Refactor to improve redability

--- lib/connect_four.ex
+++ lib/connect_four.ex
@@ -17,13 +17,18 @@ defmodule ConnectFour do
  end
 
  def check_horizontally_adjacent(board, column_index, direction) do
-  adjacent_pieces = Helpers.horizonally_adjacent_pieces(board, column_index, direction, [])
-  Helpers.is_four_in_succession?(adjacent_pieces)
+  board
+  |> Helpers.horizonally_adjacent_pieces(column_index, direction, [])
+  |> Helpers.is_four_in_succession?()
  end
 
  def check_diagonals(board, column_index) do
-   current_column = Enum.at(board, column_index)
-   move_piece_index = div(Enum.count(current_column), -1)
+   move_piece_index =
+    board
+    |> Enum.at(column_index)
+    |> Enum.count()
+    |> div(-1)
+
    check_diagonal_to_bottom_left(board, column_index, move_piece_index)
  end
 
@@ -58,7 +63,8 @@ defmodule ConnectFour do
  end
 
  def check_diagonal_to_bottom_right(board, column_index, current_piece_index) do 
-  pieces = Helpers.diagonally_adjacent_pieces(board, column_index, +1, +1, current_piece_index, [])
-  Helpers.is_four_in_succession?(pieces)
+  board
+  |> Helpers.diagonally_adjacent_pieces(column_index, +1, +1, current_piece_index, [])
+  |> Helpers.is_four_in_succession?()
  end
 end
--- lib/connect_four/helpers.ex
+++ lib/connect_four/helpers.ex
@@ -1,25 +1,31 @@
-defmodule ConnectFour.Helpers do
+  defmodule ConnectFour.Helpers do
   @moduledoc """
   Helpers for connect four game
   """
- def horizonally_adjacent_pieces(_, _, _, acc) when length(acc) == 4, do: acc 
- def horizonally_adjacent_pieces(board, current_column_index, shift, acc) do
-   current_column = Enum.at(board, current_column_index)
-   piece = Enum.at(current_column, 0)
-   next_column_index = current_column_index + shift
-   horizonally_adjacent_pieces(board, next_column_index, shift, [piece | acc])
- end
+  def horizonally_adjacent_pieces(_, _, _, acc) when length(acc) == 4, do: acc 
+  def horizonally_adjacent_pieces(board, current_column_index, shift, acc) do
+    piece =
+      board
+      |> Enum.at(current_column_index)
+      |> Enum.at(0)
+    
+    next_column_index = current_column_index + shift
+    horizonally_adjacent_pieces(board, next_column_index, shift, [piece | acc])
+  end
 
- def diagonally_adjacent_pieces(_, _, _, _, _, acc) when length(acc) == 4, do: acc
- def diagonally_adjacent_pieces(_, _, _, _, _, [_, item | _] = acc) when is_nil(item), do: acc
- def diagonally_adjacent_pieces(board, column_index, column_shift, piece_shift, current_piece_index, acc) do
-  current_column = Enum.at(board, column_index)
-  piece = Enum.at(current_column, current_piece_index)
-  next_column_index = column_index + column_shift
-  next_piece_index = current_piece_index + piece_shift
-  diagonally_adjacent_pieces(board, next_column_index, column_shift, piece_shift, next_piece_index, [piece | acc])
- end
+  def diagonally_adjacent_pieces(_, _, _, _, _, acc) when length(acc) == 4, do: acc
+  def diagonally_adjacent_pieces(_, _, _, _, _, [_, item | _] = acc) when is_nil(item), do: acc
+  def diagonally_adjacent_pieces(board, column_index, column_shift, piece_shift, current_piece_index, acc) do
+   piece =
+    board
+    |> Enum.at(column_index)
+    |> Enum.at(current_piece_index)
 
- def is_four_in_succession?([p, p, p, p]) when not is_nil(p), do: {:winner, p}
- def is_four_in_succession?(_), do: :no_winner
+    next_column_index = column_index + column_shift
+    next_piece_index = current_piece_index + piece_shift
+    diagonally_adjacent_pieces(board, next_column_index, column_shift, piece_shift, next_piece_index, [piece | acc])
+  end
+
+  def is_four_in_succession?([p, p, p, p]) when not is_nil(p), do: {:winner, p}
+  def is_four_in_succession?(_), do: :no_winner
 end
